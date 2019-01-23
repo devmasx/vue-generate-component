42e8ea0d217209a84c309aa4e5f783b77c55e3ff
Miguel Savignano <migue.masx@gmail.com>
2019-01-24 00:33:25 +0100

    test change template dir

--- test/TemplateGenerator.spec.js
+++ test/TemplateGenerator.spec.js
@@ -1,7 +1,13 @@
 const TemplateGenerator = require("../src/TemplateGenerator");
 
-new TemplateGenerator().renderTemplate(
-  `components/Component.vue.ejs`,
+// new TemplateGenerator().renderTemplate(
+//   `components/Component.vue.ejs`,
+//   "./components/Button.vue",
+//   { componentName: "Button" }
+// );
+
+new TemplateGenerator(`./test/templates_mock`).renderTemplate(
+  `Component.vue.ejs`,
   "./components/Button.vue",
   { componentName: "Button" }
 );
